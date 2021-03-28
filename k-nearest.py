b97615544b1de4cffb2179dbbf17544a8fb74e4a
unknown <piyumitha@mobisec.lk>
2021-03-29 01:49:00 +0530

    Add new models into knn

--- k-nearest.py
+++ k-nearest.py
@@ -4,16 +4,15 @@ import pickle
 import pandas as pd
 
 from sklearn import metrics
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 
 data = pd.read_csv('Label_With_Finance_Rate - Sheet1.csv')
 results = []
-with open("Label_With_Finance_Rate - Sheet1.csv") as csvfile:
-    reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)  # change contents to floats
-    for row in reader:  # each row is a list
-        results.append(row)  # shape of dataset
-        print (results)
 
 print("Shape:", data.shape)
 
@@ -45,6 +44,10 @@ print(y_test.shape)
 # training the model on training set
 
 knn = KNeighborsClassifier(n_neighbors=3)
+# knn = RandomForestClassifier(n_estimators=100)
+# knn = SVC(kernel='rbf')
+# knn = GaussianNB()
+# knn = DecisionTreeClassifier()
 knn.fit(X_train, y_train)
 
 # making predictions on the testing set
@@ -61,12 +64,17 @@ pickle.dump(knn, open(filename, 'wb'))
 # load the model from disk
 loaded_model = pickle.load(open(filename, 'rb'))
 result = loaded_model.score(X_test, y_test)
-sample = [
-    [30, 4662, 858373000, 2019, 356000000, 2.39, 181, 8.4, 23, 21, 1, 4, 14, 361, 88, 2, 40, 113, 93, 46, 0, 7, 53, 475,
-     63, 3, 29, 3, 5, 78, 2, 41, 55, 104, 11, 1, 2, 16, 343, 17, 1, 0, 1, 189, 42, 3, 3, 13],
-    [22, 4348, 334201140, 2017, 175000000, 2.39, 133, 7.4, 224, 24, 0, 0, 4, 417, 12, 0, 19, 24, 452, 78, 1, 61, 41,
-     634, 85, 2, 38, 2, 148, 43, 1, 77, 105, 321, 22, 0, 0, 4, 298, 32, 0, 0, 1, 239, 56, 0, 0, 4]]
-prediction = loaded_model.predict(sample)
+# sample = [
+#     [30, 4662, 858373000, 2019, 356000000, 2.39, 181, 8.4, 23, 21, 1, 4, 14, 361, 88, 2, 40, 113, 93, 46, 0, 7, 53, 475,
+#      63, 3, 29, 3, 5, 78, 2, 41, 55, 104, 11, 1, 2, 16, 343, 17, 1, 0, 1, 189, 42, 3, 3, 13],
+#     [22, 4348, 334201140, 2017, 175000000, 2.39, 133, 7.4, 224, 24, 0, 0, 4, 417, 12, 0, 19, 24, 452, 78, 1, 61, 41,
+#      634, 85, 2, 38, 2, 148, 43, 1, 77, 105, 321, 22, 0, 0, 4, 298, 32, 0, 0, 1, 239, 56, 0, 0, 4]]
+with open("pre.csv") as csvfile:
+    reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)  # change contents to floats
+    for row in reader:  # each row is a list
+        results.append(row)  # shape of dataset
+    print (results)
+    prediction = loaded_model.predict(results)
 print(prediction)
 print(result)
 
